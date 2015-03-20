dca49c7319a1f3a30ec518c0177c34926273c54d
Jitka Hodná <hodnajit@fel.cvut.cz>
2015-03-20 13:08:50 +0100

    OOP desigh, how will it work - ideas

---
+++ mavenproject1/src/main/java/cz/cvut/fel/pr2/Controller.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.fel.pr2;
+
+/**
+ *
+ * @author Jituška zub
+ */
+public class Controller {
+    private String username;
+    private String password;
+    Model model;
+    
+    public void moveLeft (){
+        
+    }
+    
+    public void moveRight(){
+        
+    }
+    
+    public void moveUp(){
+        
+    }
+    
+    public void moveDown(){
+        
+    }
+    
+    public void shoot(int numberOfProjectiles){
+        
+    }
+
+    public Controller(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    
+    
+}
